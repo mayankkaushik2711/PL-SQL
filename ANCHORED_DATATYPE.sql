619eb602ca07cc6f7c5aed6015861c1962b9ed16
mayank <iammayank2711@gmail.com>
2023-10-04 22:08:58 +0530

    pl-sql

--- ANCHORED_DATATYPE.sql
+++ ANCHORED_DATATYPE.sql
@@ -1,24 +1,43 @@
 SET SERVEROUTPUT ON;
 
 
---CREATE TABLE employee(first_name varchar2(20),salary number(10));
+
+--DECLARE 
+--    v_name employee.first_name%type;
 --
---INSERT INTO employee VALUES('MAYANK',1000);
-
-
---DECLARE
---     v_name varchar(20);
---    
---    
+--
 --BEGIN
---    SELECT first_name INTO v_name FROM employee WHERE first_name='MAYANK';
---    DBMS_OUTPUT.PUT_LINE(v_name);
---END;
+--
+--    SELECT first_name INTO v_name FROM employee WHERE salary=1000;
+--    dbms_output.put_line(v_name);
+--    
+--end;
 --/
---
-
---
---....SELECT * FROM SYSTEM.employee;
 
 
+alter table employee add age number(2);
 
+UPDATE employee SET age=20 WHERE salary=1000;
+
+select * from employee;
+
+insert into employee values('ata',2000,19);
+INSERT INTO employee VALUES ('John', 3000, 25);
+
+-- Insert Row 2
+INSERT INTO employee VALUES ('Alice', 2500, 28);
+
+-- Insert Row 3
+INSERT INTO employee VALUES ('Bob', 3500, 32);
+
+-- Insert Row 4
+INSERT INTO employee VALUES ('Eva', 2800, 22);
+
+-- Insert Row 5
+INSERT INTO employee VALUES ('Mike', 4000, 27);
+
+select * from employee;
+
+
+
+    
